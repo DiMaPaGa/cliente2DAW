8e0c8f59239642bced1856b56799c46763c34be8
DiMaPaGa <145446082+DiMaPaGa@users.noreply.github.com>
2024-10-22 12:43:36 +0200

    Entrega de la actividad 11 de cliente tras aclaraciones de Joaquin

--- Ejercicio11/src/main/java/com/vedruna/ejemplo/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.vedruna.ejemplo.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-    
-}
